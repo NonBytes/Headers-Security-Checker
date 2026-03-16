097531e630f6b3538f04e8021d2d9fed175c0298
NonBytes <thai_hacker@hotmail.com>
2026-03-16 11:16:45 +0700

    chore: update Homebrew formula to v0.1.3

--- Formula/headers-security-checker.rb
+++ Formula/headers-security-checker.rb
@@ -1,14 +1,14 @@
 class HeadersSecurityChecker < Formula
   desc "High-performance command-line tool for security auditing of HTTP response headers"
   homepage "https://github.com/NonBytes/Headers-Security-Checker"
-  version "0.1.2"
+  version "0.1.3"
 
   if OS.mac?
-    url "https://github.com/NonBytes/Headers-Security-Checker/releases/download/v0.1.2/hsc-macos-amd64"
-    sha256 "17bfd36ebd5a5c2c7dcf1b9f7d066119ca4133adb6c28bf8b558f6651bdecba8"
+    url "https://github.com/NonBytes/Headers-Security-Checker/releases/download/v0.1.3/hsc-macos-amd64"
+    sha256 "108c11d7692d0a2cffa4ce62a8dbc9d115ad44ca276669da0809809520916cf5"
   elsif OS.linux?
-    url "https://github.com/NonBytes/Headers-Security-Checker/releases/download/v0.1.2/hsc-linux-amd64"
-    sha256 "17bfd36ebd5a5c2c7dcf1b9f7d066119ca4133adb6c28bf8b558f6651bdecba8"
+    url "https://github.com/NonBytes/Headers-Security-Checker/releases/download/v0.1.3/hsc-linux-amd64"
+    sha256 "66f0d5f867491bec308f453943ab18ade098b2401ac8bb1f4c90a54892b6bf3b"
   end
 
   def install
